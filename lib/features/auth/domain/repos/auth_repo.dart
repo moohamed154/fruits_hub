cb4043261c1ae4a7af8659bdc19467522057c790
moohamed154 <medohs770@gmail.com>
2025-02-22 00:06:52 +0200

    Add dartz package and implement authentication repository with user entity and error handling

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failure.dart';
+import 'package:fruits_hub/features/auth/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+}
