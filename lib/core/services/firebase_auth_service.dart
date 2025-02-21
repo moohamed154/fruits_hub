d4d3ad18b6207f62e0821f7349619e4e00b99332
moohamed154 <medohs770@gmail.com>
2025-02-22 00:40:28 +0200

    Implement Firebase authentication service and user model, update auth repository to include name parameter

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+
+class FirebaseAuthService {
+  Future<User> createUserWithEmAilAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException(message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+            message: 'The account already exists for that email.');
+      } else {
+        throw CustomException(
+            message: 'An error occurred. Please try again later.');
+      }
+    } catch (e) {
+      throw CustomException(
+          message: 'An error occurred. Please try again later.');
+    }
+  }
+}
---
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+import 'package:fruits_hub/core/errors/failure.dart';
+import 'package:fruits_hub/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub/features/auth/data/models/user_model.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/features/auth/entities/user_entity.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthRepoImpl({required this.firebaseAuthService});
+  @override
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      String email, String password, String name) async {
+    try {
+      var user = await firebaseAuthService.createUserWithEmAilAndPassword(
+          email: email, password: password);
+      return Right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          'An error occurred. Please try again later.',
+        ),
+      );
+    }
+  }
+}
