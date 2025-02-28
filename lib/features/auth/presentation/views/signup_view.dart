a7f8c57d104e5e041b25a56c4e6e5cf96deb5435
moohamed154 <medohs770@gmail.com>
2025-02-28 20:32:04 +0200

    Implement dependency injection for SignupCubit and update pubspec.yaml for new dependencies

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubits/signup_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
+import 'package:fruits_hub/features/auth/repos/auth_repo.dart';
 
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
   static const routeName = 'signup';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(
-        context: context,
-        title: 'حساب جديد',
+    return BlocProvider(
+      create: (context) => SignupCubit(
+        getIt<AuthRepo>(),
       ),
-      body: const SignupViewBody(),
+      child: Scaffold(
+        appBar: buildAppBar(
+          context: context,
+          title: 'حساب جديد',
+        ),
+        body: const SignupViewBody(),
+      ),
     );
   }
 }
