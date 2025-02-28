d26ec9250a2c91df21f27c2d422c87096b4d1fb1
moohamed154 <medohs770@gmail.com>
2025-03-01 01:25:01 +0200

    Add PasswordField widget for improved password input handling; refactor SignupViewBody to utilize PasswordField and enhance CustomTextFormField with obscureText functionality

--- lib/core/widgets/custom_text_field.dart
+++ lib/core/widgets/custom_text_field.dart
@@ -7,15 +7,18 @@ class CustomTextFormField extends StatelessWidget {
       required this.hintText,
       required this.textInputType,
       this.suffixIcon,
-      this.onSaved});
+      this.onSaved,
+      this.obscureText = false});
 
   final String hintText;
   final TextInputType textInputType;
   final Widget? suffixIcon;
   final void Function(String?)? onSaved;
+  final bool obscureText;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: obscureText,
       onSaved: onSaved,
       validator: (value) {
         if (value == null || value.isEmpty) {
