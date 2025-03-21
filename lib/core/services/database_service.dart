58e4d2fedaaa03db7e02f8ecced2c0347c3eb903
moohamed154 <medohs770@gmail.com>
2025-03-21 21:13:40 +0200

    Add getData method to DatabaseService and FirestoreService; implement data retrieval from Firestore and add fromJson method to UserModel for JSON parsing.

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,4 +1,8 @@
 abstract class DatabaseService {
   Future<void> addData(
       {required String path, required Map<String, dynamic> data});
+  Future<Map<String, dynamic>> getData({
+    required String path,
+    required String documentId,
+  });
 }
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -9,4 +9,11 @@ class FirestoreService implements DatabaseService {
       {required String path, required Map<String, dynamic> data}) async {
     await firestore.collection(path).add(data);
   }
+
+  @override
+  Future<Map<String, dynamic>> getData(
+      {required String path, required String documentId}) async {
+    var data = await firestore.collection(path).doc(documentId).get();
+    return data.data() as Map<String, dynamic>;
+  }
 }
