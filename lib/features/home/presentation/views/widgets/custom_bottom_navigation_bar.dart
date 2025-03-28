7162fea4f60cede224bdfefb51ee1d9923d79473
moohamed154 <medohs770@gmail.com>
2025-03-28 20:10:25 +0200

    Refactor ActiveItem and InActiveItem widgets for improved layout; update CustomBottomNavigationBar to enhance item selection handling

--- lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
@@ -38,9 +38,19 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
         children: bottomNavigationBarItems.asMap().entries.map((e) {
           var index = e.key;
           var entity = e.value;
-          return NvigationBarItem(
-            isSelected: selectedIndex == index,
-            bottomNavigationBarEntity: entity,
+          return Expanded(
+            flex: index == selectedIndex ? 3 : 2,
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: NvigationBarItem(
+                isSelected: selectedIndex == index,
+                bottomNavigationBarEntity: entity,
+              ),
+            ),
           );
         }).toList(),
       ),
